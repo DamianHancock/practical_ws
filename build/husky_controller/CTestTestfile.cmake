d3e5f14c29a4f3d3be3187ec54f74ba02c97f7dd
DamianHancock <64782797+DamianHancock@users.noreply.github.com>
2022-02-08 21:20:28 +0800

    Delete build directory

--- build/husky_controller/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/damian/Chironix_Practical/src/simulation_control/src/husky_controller
-# Build directory: /home/damian/Chironix_Practical/build/husky_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
